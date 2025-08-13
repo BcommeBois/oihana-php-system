7f91ac9db15c36ccf89c914e1ac09e1dcc5dd902
Marc Alcaraz <ekameleon@gmail.com>
2025-08-13 16:49:03 +0200

    Fix the HttpMethod enumeration class

--- src/oihana/http/HttpMethod.php
+++ src/oihana/http/HttpMethod.php
@@ -37,4 +37,23 @@ class HttpMethod
 
     public const string TRACE = 'TRACE';
     public const string trace = 'trace';
+
+    // ------ extras
+
+    public const string ALL      = 'ALL' ;
+    public const string CONNECT  = 'CONNECT';
+
+    public const string all       = 'all' ;
+    public const string connect   = 'connect';
+    public const string default   = 'default' ;
+    public const string deleteAll = 'deleteAll' ;
+    public const string count     = 'count' ;
+    public const string exist     = 'exist' ;
+    public const string flush     = 'flush' ;
+    public const string insert    = 'insert' ;
+    public const string list      = 'list' ;
+    public const string replace   = 'replace' ;
+    public const string truncate  = 'truncate' ;
+    public const string update    = 'update' ;
+    public const string upsert    = 'upsert' ;
 }
