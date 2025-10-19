131962ff7a849484a23cf5d16d4a2f364a70b925
Marc Alcaraz <ekameleon@gmail.com>
2025-10-19 08:09:36 +0200

    Adds the oihana/models/helpers/cacheCollection function
    Requires the matthiasmullie/scrapbook library !

---
+++ src/oihana/models/helpers/cacheCollection.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace oihana\models\helpers ;
+
+use DI\Container;
+use DI\DependencyException;
+use DI\NotFoundException;
+
+use MatthiasMullie\Scrapbook\KeyValueStore;
+use MatthiasMullie\Scrapbook\Psr16\SimpleCache;
+
+/**
+ * Creates a namespaced cache collection from a Memcached store definition
+ * registered in the dependency injection container.
+ *
+ * A cache collection is an isolated namespace within the same backend,
+ * allowing logical separation of cached values (e.g. per feature, domain, or module).
+ * This helper function retrieves a {@see KeyValueStore} from the container,
+ * and wraps its collection in a PSR-16 {@see SimpleCache} implementation.
+ *
+ * Example usage:
+ * ```php
+ * // Retrieve a cache collection named "users"
+ * $userCache = cacheCollection( $container , "users" , 'cache:memory' );
+ *
+ * // Store and retrieve values
+ * $userCache->set("id:42", ["name" => "Alice"]);
+ * $data = $userCache->get("id:42");
+ * ```
+ *
+ * @param Container $container  The DI container used to resolve the cache store definition.
+ * @param string    $collection The collection name (namespace) to create inside the cache store.
+ * @param string    $definition The container entry identifier of the base key/value store in the DI container.
+ *
+ * @return SimpleCache|null A PSR-16 cache instance scoped to the given collection, or `null` if the definition is not found or not compatible.
+ *
+ * @throws DependencyException If the container fails to resolve the cache definition.
+ * @throws NotFoundException   If the cache definition is not registered in the container.
+ *
+ * @see https://www.scrapbook.cash
+ */
+function cacheCollection
+(
+    Container $container  ,
+    string    $collection ,
+    string    $definition
+)
+: ?SimpleCache
+{
+    if( $container->has( $definition ) )
+    {
+        $cache = $container->get( $definition ) ;
+        if( $cache instanceof KeyValueStore )
+        {
+            return new SimpleCache( $cache->getCollection( $collection ) ) ;
+        }
+    }
+    return null ;
+}
---
+++ tests/oihana/models/helpers/CacheCollectionTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace tests\oihana\models\helpers;
+
+use DI\DependencyException;
+use DI\NotFoundException;
+use PHPUnit\Framework\TestCase;
+
+use DI\Container;
+use MatthiasMullie\Scrapbook\KeyValueStore;
+use MatthiasMullie\Scrapbook\Psr16\SimpleCache;
+
+use stdClass;
+
+use function oihana\models\helpers\cacheCollection;
+
+
+class CacheCollectionTest extends TestCase
+{
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    public function testReturnsSimpleCacheWhenDefinitionIsValid(): void
+    {
+        $collectionName = 'users';
+
+        // KeyValueStore Mock
+        $store = $this->createMock(KeyValueStore::class ) ;
+
+        $store->expects    ( $this->once() )
+              ->method     ('getCollection' )
+              ->with       ( $collectionName )
+              ->willReturn ( $store ); // Scrapbook returns a KeyValueStore
+
+        // Mock du container
+        $container = $this->createMock(Container::class);
+
+        $container->expects($this->once())
+            ->method('has')
+            ->with('cache:memory')
+            ->willReturn(true);
+
+        $container->expects($this->once())
+            ->method('get')
+            ->with('cache:memory')
+            ->willReturn($store);
+
+        $cache = cacheCollection($container, $collectionName, 'cache:memory');
+
+        $this->assertInstanceOf(SimpleCache::class, $cache);
+    }
+
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    public function testReturnsNullWhenDefinitionDoesNotExist(): void
+    {
+        $container = $this->createMock( Container::class ) ;
+
+        $container->expects($this->once())
+                  ->method('has')
+                  ->willReturn(false);
+
+        $cache = cacheCollection($container, 'test', 'cache:unknown');
+        $this->assertNull($cache);
+    }
+
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     */
+    public function testReturnsNullWhenDefinitionIsNotKeyValueStore(): void
+    {
+        $container = $this->createMock( Container::class ) ;
+        $container->expects($this->once())
+                  ->method('has')
+                  ->willReturn(true);
+
+        $container->expects($this->once())
+                  ->method('get')
+                  ->willReturn(new stdClass()) ; // not a KeyValueStore
+
+        $cache = cacheCollection( $container , 'invalid' , 'cache:memory') ;
+        $this->assertNull( $cache ) ;
+    }
+}
