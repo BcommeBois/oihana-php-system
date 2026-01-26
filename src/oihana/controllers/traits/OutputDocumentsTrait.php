74553955f4c2234573b5d9434753c243157cec68
Marc Alcaraz <ekameleon@gmail.com>
2026-01-26 10:08:22 +0100

    Update OutputDocumentsTrait.php

--- src/oihana/controllers/traits/OutputDocumentsTrait.php
+++ src/oihana/controllers/traits/OutputDocumentsTrait.php
@@ -7,21 +7,36 @@ use oihana\enums\Output;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
+/**
+ * Provides a standardized way to output documents from controllers.
+ *
+ * This trait offers methods to format documents responses, generate
+ * URLs for documents, and handle optional response wrapping with PSR-7
+ * response objects. It relies on BaseUrlTrait for URL generation and
+ * StatusTrait for response formatting.
+ *
+ * Usage example:
+ * ```php
+ * $documents = $this->outputDocuments($request, $response, $data, ['page' => 1]);
+ * ```
+ */
 trait OutputDocumentsTrait
 {
     use BaseUrlTrait ,
         StatusTrait ;
 
     /**
-     * The internal documents response returns strategy.
+     * Generates a standardized response for a list of documents.
      *
-     * @param Request|null $request
-     * @param Response|null $response
-     * @param array|null $documents
-     * @param array $params
-     * @param array|null $options
+     * Wraps the given documents in a success response, including count, options, and a document URL.
      *
-     * @return ?object
+     * @param Request|null  $request   Optional PSR-7 request object.
+     * @param Response|null $response  Optional PSR-7 response object.
+     * @param array|null    $documents Optional array of documents to include in the response.
+     * @param array         $params    Optional parameters used for URL generation.
+     * @param array|null    $options   Optional additional options to include in the response.
+     *
+     * @return object|null Returns a success-wrapped object if $response is provided; otherwise null.
      */
     protected function documentsResponse( ?Request $request = null, ?Response $response = null , ?array $documents = null , array $params = [] , ?array $options = null ) :?object
     {
@@ -34,28 +49,36 @@ trait OutputDocumentsTrait
     }
 
     /**
-     * Invoked in the documentsResponse method to returns the document url.
+     * Returns the URL associated with the documents response.
      *
-     * By default, the method use the BaseUrlTrait::getCurrentPath() method, you can overrides it.
+     * By default, this method uses BaseUrlTrait::getCurrentPath() but can
+     * be overridden in the controller to provide a custom URL generation logic.
      *
-     * @param ?Request $request
-     * @param array $params
+     * @param Request|null $request Optional PSR-7 request object.
+     * @param array        $params  Optional parameters to append to the URL.
      *
-     * @return string
+     * @return string The generated document URL.
      */
     protected function getDocumentUrl( ?Request $request = null , array $params = [] ) :string
     {
         return $this->getCurrentPath( $request , $params ) ;
     }
 
+
     /**
-     * Outputs a list of documents.
-     * @param ?Request $request
-     * @param ?Response|null $response
-     * @param ?array $documents
-     * @param array $params
-     * @param ?array $options
-     * @return array|object|null
+     * Outputs a list of documents, optionally wrapping them in a response object.
+     *
+     * If a PSR-7 Response object is provided, the documents are wrapped using
+     * `documentsResponse()`. Otherwise, the raw array of documents is returned.
+     *
+     * @param Request|null  $request   Optional PSR-7 request object.
+     * @param Response|null $response  Optional PSR-7 response object.
+     * @param array|null    $documents Optional array of documents to output.
+     * @param array         $params    Optional parameters for URL generation.
+     * @param array|null    $options   Optional additional response options.
+     *
+     * @return array|object|null Returns a wrapped response object if $response is provided,
+     *                           otherwise the raw documents array or null.
      */
     protected function outputDocuments
     (
